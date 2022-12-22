71671d7b16f0895b201d895c345ddc9112bc5e33
PirateGamers <109447641+PirateGamers@users.noreply.github.com>
2022-12-22 13:29:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,8 +48,8 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Download_Movies_In_Hind
 MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 
 # Custom Chats
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001786369133)
-FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001373716687)
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001786369133))
+FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001373716687))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 't.me/Technical_Bots')
 HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/How_To_Open_Links_23')
 
